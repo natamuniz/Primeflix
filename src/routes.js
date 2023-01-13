493e04d62c0d5dcc982152bd6a685d81db2d374e
Natã Muniz <natan.m25@gmail.com>
2023-01-13 07:52:20 -0300

    Adding favourites route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import Home from './pages/Home';
 import Filmes from './pages/Filmes';
+import Favoritos from './pages/Favoritos';
 
 import Erro from './pages/Error';
 
@@ -16,6 +17,7 @@ function RoutesApp(){
           <Routes>
             <Route path="/" element={ <Home/> } />
             <Route path="/filme/:id" element={ <Filmes/> } />
+            <Route path='/favoritos' element={ <Favoritos/> } />
 
             <Route path='*' element={ <Erro/> } />
           </Routes>
